c171a311bad4f47f452d321948dea6489952ceef
amita@railsfactory.org <chaudhary.amita27@gmail.com>
2013-01-11 15:56:55 +0530

    My Commit

--- OBM/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-OBM::Application.config.secret_token = 'c75d4988a7aea0b10216b5b62b4a8912fc124c4b9e103a25ffadeb55d544e23eca4da8e43bae1853f5f6484fcac30f6d3e37ce5c4646b7ef8b5ecf5a3b392095'
