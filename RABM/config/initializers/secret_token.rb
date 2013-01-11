60c5d4d1e5317e077752b756cecfc8285c1cf9a5
df-rakesh <rakesh.c@railsfactory.org>
2013-01-11 15:45:19 +0530

    structure created

---
+++ RABM/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RABM::Application.config.secret_token = '60253cead3a1951118be3cf9bf9a7d425b129824d6a74b982986fc50be5f49a1057f4ba4294586476742aba41c86e8154f1d117c9e2e83a0d740b00fefab7a18'
